86dc8f6f8486f2b06d66581fef481980526e5559
AugustoCiuffoletti <augusto@di.unipi.it>
2022-03-30 13:04:22 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import { Observable } from 'rxjs';
 import { ajax, AjaxResponse, AjaxRequest, AjaxError } from 'rxjs/ajax';
 const URL: string =
   'https://eu-central-1.aws.data.mongodb-api.com/app/kvaas-giwjg/endpoint';
-var key="";
+var key="e3ce9512";
 document.getElementById('getbtn').addEventListener('click', getValue);
 
 function getValue() {
