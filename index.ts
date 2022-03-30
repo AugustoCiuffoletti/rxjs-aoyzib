eaac8382388b5ac2247566bd457b97bb77910f82
AugustoCiuffoletti <augusto@di.unipi.it>
2022-03-30 13:37:54 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -3,18 +3,18 @@ import { Observable } from 'rxjs';
 import { ajax, AjaxResponse, AjaxRequest, AjaxError } from 'rxjs/ajax';
 const URL: string =
   'https://eu-central-1.aws.data.mongodb-api.com/app/kvaas-giwjg/endpoint';
-var key;
+var key: string;
 document.getElementById('newbtn').addEventListener('click', newKey);
 document.getElementById('setbtn').addEventListener('click', setValue);
 document.getElementById('getbtn').addEventListener('click', getValue);
 
 function newKey() {
-  const request: AjaxRequest = {
+  const obs = ajax({
     method: 'GET',
     url: URL + '/new?secret=ssw2022',
     crossDomain: true,
-  };
-  ajax(request).subscribe({
+  })
+  obs.subscribe({
     next: (res: AjaxResponse<any>) => {
       key = res.response;
       document.getElementById('key').innerHTML = key;
@@ -23,31 +23,31 @@ function newKey() {
   });
 }
 
-function setValue() {
-  const request: AjaxRequest = {
-    method: 'POST',
-    url:URL + '/set?key=' + key,
+function getValue() {
+  const obs = ajax({
+    method: 'GET',
+    url: URL + '/get?key=' + key,
     crossDomain: true,
-    body: document.getElementById('data').value
-  };
-  console.log(document.getElementById('data'));
-  ajax(request).subscribe({
+  });
+  obs.subscribe({
     next: (res: AjaxResponse<any>) => {
-      document.getElementById('output').innerHTML = 'Ok!';
+      document.getElementById('output').innerHTML = res.response;
     },
     error: (err: AjaxError) => console.error(err.response),
   });
 }
 
-function getValue() {
-  const request: AjaxRequest = {
-    method: 'GET',
-    url: URL + '/get?key=' + key,
+function setValue() {
+  console.log(document.getElementById('data'));
+  const obs = ajax({
+    method: 'POST',
+    url:URL + '/set?key=' + key,
     crossDomain: true,
-  };
-  ajax(request).subscribe({
+    body: document.getElementById('data').value
+  })
+  obs.subscribe({
     next: (res: AjaxResponse<any>) => {
-      document.getElementById('output').innerHTML = res.response;
+      document.getElementById('output').innerHTML = 'Ok!';
     },
     error: (err: AjaxError) => console.error(err.response),
   });
